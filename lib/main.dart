deeb2b97146fde0aa8ea67a334baa4ab1a71c0db
shawn <shawnfeng610@gmail.com>
2022-09-26 14:26:15 +0800

    add background color, text style, img

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,30 @@ import 'package:flutter/material.dart';
 void main() {
   var appTitle = Text('我的第一個Flutter App'),
       hiFlutter = Text(
-        'Hi, Flutter.' ,
-        style: TextStyle(fontSize: 50),
+        "Hi, Flutter.\n今天是220926" ,
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Colors.red,
+          decoration:TextDecoration.underline,
+        ),
       );
-
+  var img = Image.network("https://i.epochtimes.com/assets/uploads/2021/08/id13156667-shutterstock_376153318-600x400.jpg");
   var appBody = Center(
-    child: hiFlutter,
+    //child:hiFlutter,
+    child: img,
   );
 
   var appBar = AppBar(
     title: appTitle,
+    backgroundColor: Colors.lightBlue[400],
   );
 
   var app = MaterialApp(
     home: Scaffold(
       appBar: appBar,
       body: appBody,
+      backgroundColor: Color(0xFFFAEEE7),
     ),
   );
 
