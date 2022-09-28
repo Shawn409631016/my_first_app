3b7644fd58092b3c35dfd97a4359167298c2a2da
shawn <shawnfeng610@gmail.com>
2022-09-28 16:16:44 +0800

    add Container

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   var appTitle = Text('我的第一個Flutter App'),
       hiFlutter = Text(
-        "Hi, Flutter.\n今天是220926" ,
+        "Hi, Flutter.\n今天是220928" ,
         style: TextStyle(
           fontSize: 30,
           fontWeight: FontWeight.bold,
@@ -13,8 +13,27 @@ void main() {
       );
   var img = Image.network("https://i.epochtimes.com/assets/uploads/2021/08/id13156667-shutterstock_376153318-600x400.jpg");
   var appBody = Center(
-    //child:hiFlutter,
-    child: img,
+
+    // child: img,
+
+    // child: Container(
+    //   margin: const EdgeInsets.all(10.0),
+    //   color: Colors.white,
+    //   width: 400.0,
+    //   height: 560.0,
+    //   child: hiFlutter,
+    // ),
+
+    child: Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        color: Colors.red[600],
+        width: 48.0,
+        height: 48.0,
+      ),
+    ),
+
   );
 
   var appBar = AppBar(
@@ -26,9 +45,10 @@ void main() {
     home: Scaffold(
       appBar: appBar,
       body: appBody,
-      backgroundColor: Color(0xFFFAEEE7),
+      backgroundColor: Color(0xFFFFF7D5),
     ),
   );
+
 
   runApp(app);
 }
